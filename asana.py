34913450a01383d0b4438dfdcb3360cf80a163b5
Kamel TIGHIDET <tighidet.kamel@gmail.com>
2016-05-07 01:11:10 +0200

    workspace getter

--- asana.py
+++ asana.py
@@ -13,13 +13,43 @@ By Kamel TIGHIDET <tighidet.kamel@gmail.com>
 import base64
 import requests
 import time
-from urllib.parse import quote
-import simplejson as json
-from asana_token import token
+from urllib import quote
+import json
+# from asana_params import token
+import asana_params
+
 
 
 
 class asanaAPI():
 
-	def __init__(self, token):
+	def __init__(self):
+		self.asana_api_url = "https://app.asana.com/api/1.0"
+		self.token = asana_params.token
+		self.wrkspace_name = asana_params.workspace_name
+		self.auth_key = base64.b64encode(self.token + ":")
 
+
+	# @classmethod
+	def asana(self):
+		workspace_id = self.get_workspace_id()
+		if(not workspace_id):
+			return None
+		# while True:
+
+	def get_workspace_id(self):
+		workspaces_target = ('workspaces')
+		workspaces_req = requests.get("/".join([self.asana_api_url, quote(workspaces_target, safe="/&=?")]), auth=(self.token, ""))		
+		found_ws = False
+		for ws in json.loads(workspaces_req.text)['data']:
+			if(ws['name'] == self.wrkspace_name):
+				 workspace_id = ws['id']
+				 found_ws = True
+				 print 'Found workspace | id = ' + str(workspace_id)
+
+		if(not found_ws):
+			print 'workspace not found'
+			return None
+
+toto = asanaAPI()
+toto.asana()
