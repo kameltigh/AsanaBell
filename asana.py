97530bd79f9af6347e5477e42a188abdc65648d5
Kamel TIGHIDET <tighidet.kamel@gmail.com>
2016-05-22 23:56:31 +0200

    managing multiple projects

--- asana.py
+++ asana.py
@@ -19,9 +19,9 @@ from urllib import quote
 import json
 import asana_params
 import datetime
-import eqsbell as bell
+# import eqsbell as bell
 
-
+# TODO init workspaces befor starting
 
 
 class asanaAPI():
@@ -32,8 +32,10 @@ class asanaAPI():
 		self.wrkspace_name = asana_params.workspace_name
 		self.project_subname = asana_params.project_subname
 		self.auth_key = base64.b64encode(self.token + ":")
-		self.nb_uncompleted_tasks = -1
+		self.nb_init_projects = 0
+		self.nb_projects = 0
 		self.uncompleted_tasks = {}
+		self.completed_eventually = {}
 
 
 
@@ -41,13 +43,15 @@ class asanaAPI():
 	def asana(self):
 		workspace_id = self.get_workspace_id()
 		projects_ids = self.get_projects_id()
+		self.nb_init_projects = len(projects_ids)
+		self.nb_projects = len(projects_ids)
 		while True:
 			if((not workspace_id) or (not projects_ids)):
 				# return None
 				print 'Error | '+str(workspace_id)+" | "+str(projects_ids)
 			else:
 				for project_id in projects_ids:
-					self.get_validated_tasks(workspace_id, project_id)
+					self.run(workspace_id, project_id)
 
 	def get_workspace_id(self):
 		workspaces_target = 'workspaces'
@@ -79,35 +83,56 @@ class asanaAPI():
 			return None
 		return projects_ids
 
-	def get_validated_tasks(self, workspace_id, project_id):
+	def run(self, workspace_id, project_id):
 		validation_time = datetime.datetime.now().isoformat()[:-3]+'Z'
-		tasks_target = 'tasks?opt_fields=completed&workspace=%d&project=%d&completed_since=%s&include_archived=%s' % (workspace_id, project_id, validation_time, 'true')
+		tasks_target = 'tasks?opt_fields=completed,name&opt_pretty&limit=100&workspace=%d&project=%d&completed_since=%s&include_archived=%s' % (workspace_id, project_id, validation_time, 'true')
 		tasks_req = requests.get("/".join([self.asana_api_url, quote(tasks_target, safe="/&=?")]), auth=(self.token, ""))		
 		found_tasks = False
 		tasks_ids = []
 		# We init the tasks dict
-		if len(self.uncompleted_tasks) == 0:
+		if self.nb_init_projects > 0:
+			self.nb_init_projects -= 1
 			for task in json.loads(tasks_req.text)['data']:
 				self.uncompleted_tasks[task['id']] = task['completed']
 			return
 		# Check for completed tasks
+		print json.loads(tasks_req.text)['data']
 		for task_id, m_task_completed in self.uncompleted_tasks.iteritems():
+			print self.completed_eventually
 			if m_task_completed:
 				continue
-			is_completed = not m_task_completed
+			is_newly_completed = True
+			found_task = False
 			for snap_task in json.loads(tasks_req.text)['data']:
 				if snap_task['id'] == task_id:
+					found_task = True
 					if snap_task['completed'] == False:
-						is_completed = False
-			if is_completed:
-				print "jingle_bell!!!"
-				bell.jingle_bell()
-				self.uncompleted_tasks[task_id] = True
-				time.sleep(3)
+						is_newly_completed = False			
+			# if not found_task:
+			# 	print "found_task = "+str(found_task)+" is_newly_completed = "+str(is_newly_completed)
+			if not found_task and is_newly_completed:
+				if task_id in self.completed_eventually:
+					if self.completed_eventually[task_id] == 0:
+						print "Validated task : "+str(task_id)
+						del self.completed_eventually[task_id]
+						self.uncompleted_tasks[task_id] = True
+						# bell.jingle_bell()
+						time.sleep(3)
+					elif self.completed_eventually[task_id] > 0:
+						self.completed_eventually[task_id] -= 1
+						# print "Not found in a project"
+				else:
+					self.completed_eventually[task_id] = self.nb_projects
+			else:
+				if task_id in self.completed_eventually:
+					del self.completed_eventually[task_id]
 		# We add new tasks to the dict
 		for task in json.loads(tasks_req.text)['data']:
 			if not task['id'] in self.uncompleted_tasks.keys():
 				self.uncompleted_tasks[task['id']] = task['completed']
+				print "Added task: "+str(task['id'])
+		print 'still have %u uncompleted tasks' % len(self.uncompleted_tasks)
+		print self.uncompleted_tasks
 
 
 test_instance = asanaAPI()
